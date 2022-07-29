17c962ede7c8c304428f23252ddebd40da793ca8
vikasrohra <vikasrohra2@gmail.com>
2022-07-30 02:03:16 +0530

    Updated author in package.json and cropped some project images

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -49,8 +49,8 @@ const IndexPage = ({ data }) => {
                 </ResumeArea>
                 {/* <TestimonialArea data={content["testimonial-section"]} /> */}
                 <ClientArea data={content["client-section"]} />
-                {/* <PricingArea data={content["pricing-section"]} />
-                <BlogArea
+                {/* <PricingArea data={content["pricing-section"]} /> */}
+                {/* <BlogArea
                     data={{
                         ...content["blog-section"],
                         blogs: data?.allArticle?.nodes,
