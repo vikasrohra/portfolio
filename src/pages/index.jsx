79f9bf35e5ed6f3699ca7f4e3c3adaaeac9b99a4
vikasrohra <vikasrohra2@gmail.com>
2022-08-14 03:23:41 +0530

    Changed page title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -22,7 +22,7 @@ import InterviewArea from "@containers/interview/layout-01";
 const IndexPage = ({ data }) => {
     const content = normalizedData(data?.homePage?.content || []);
     return (
-        <Layout pageTitle="Home Default" className="white-version">
+        <Layout pageTitle="Home" className="white-version">
             <Header
                 data={{
                     ...data.header,
