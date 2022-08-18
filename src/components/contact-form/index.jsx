23194bf7a8b19728eb798411a36c00153ea32325
vikasrohra <vikasrohra2@gmail.com>
2022-08-18 19:43:33 +0530

    Removed id attribute from contact form

--- src/components/contact-form/index.jsx
+++ src/components/contact-form/index.jsx
@@ -64,7 +64,6 @@ const ContactForm = ({ className, url }) => {
                     netlify-honeypot="bot-field"
                     data-netlify="true"
                     className="rnt-contact-form rwt-dynamic-form row"
-                    id="contact-form"
                     // onSubmit={handleSubmit(onSubmit)}
                     onSubmit="submit"
                 >
